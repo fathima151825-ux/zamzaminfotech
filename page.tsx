ad3cc00d0b8cc7de4cba736c7c92f0337f6f90fc
fathima151825-ux <fathima151825@gmail.com>
2026-04-19 22:43:10 +0530

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,22 +1,5 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import HeroSection from './components/HeroSection';
-import FeaturesSection from './components/FeaturesSection';
-import ProductsPreview from './components/ProductsPreview';
-import ServicesHighlight from './components/ServicesHighlight';
-import CTASection from './components/CTASection';
+import { redirect } from 'next/navigation';
 
-export default function HomepagePage() {
-  return (
-    <main className="bg-background text-foreground overflow-x-hidden">
-      <Header />
-      <HeroSection />
-      <FeaturesSection />
-      <ProductsPreview />
-      <ServicesHighlight />
-      <CTASection />
-      <Footer />
-    </main>
-  );
+export default function Page() {
+  redirect('/homepage');
 }
